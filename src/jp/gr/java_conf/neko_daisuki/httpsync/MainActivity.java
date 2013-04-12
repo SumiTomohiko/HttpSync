beb873ea21c67b79f407501f5753c40c38264888
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-04-13 01:50:55 +0900

    Added AboutActivity to show version information.

--- src/jp/gr/java_conf/neko_daisuki/httpsync/MainActivity.java
+++ src/jp/gr/java_conf/neko_daisuki/httpsync/MainActivity.java
@@ -31,6 +31,7 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -440,6 +441,12 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent i = new Intent(this, AboutActivity.class);
+        this.startActivity(i);
         return true;
     }
 
